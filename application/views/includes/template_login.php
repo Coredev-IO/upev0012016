f177d53b629c8e60145ddf1a3fd9db6a35a84dcd
Octavio Martinez Campuzano <octavio.martinez@coredev.io>
2016-04-07 04:22:45 -0500

    Login interfaz

--- application/views/includes/template_login.php
+++ application/views/includes/template_login.php
@@ -1,12 +1,34 @@
 <!DOCTYPE html>
 <html lang="es">
 <?php $this->load->view('includes/head')?>
-        <body>
+<body>
+        <div class="header-ipn">
+        	<div class="container">
+        		<div class="col-md-6">
+					<img src="<?=base_url()?>/principal/img/sep.png" alt="">
+        		</div>
+        		<div class="col-md-6 right">
+					<img src="<?=base_url()?>/principal/img/ipn.png" alt="">
+        		</div>
+        	</div>
+        </div>
+        <div class="menu-top">
+        <center>Sistema Institucional de Evaluación del Desempeño de Las Unidades Académicas</center>
+
+        </div>
+        <div class="menu-cont">
+
+       <div class="container">
+        	<div class="menu-op">
+
+        	</div></div>
+
+        </div>
                  <div class="container cnt-body">
-                        <?php
-                                $this->load->view($main_cont);
-                                $this->load->view('includes/js');
-                        ?>
-                </div>
+<?php
+$this->load->view($main_cont);
+$this->load->view('includes/js');
+?>
+</div>
          </body>
 </html>
--- application/views/login/index.php
+++ application/views/login/index.php
@@ -4,7 +4,7 @@
                 <div class="well">
                         <div class="form-group label-floating is-empty">
                                 <label for="username" class="control-label">Username</label>
-                                <input type="text" class="form-control" id="username" name="username">
+                                <input type="text" class="form-control" id="username" name="username" autofocus="true">
                                 <span class="help-block">Usuario proporcionado por la UPEV </span>
                         </div>
                         <div class="form-group label-floating is-empty">
@@ -12,16 +12,16 @@
                                 <input type="password" class="form-control" id="password" name="password">
                                 <span class="help-block">Si olvido la contraseña contacte al administrador</span>
                         </div>
-                        <hr>
                         <button type="submit" name="btn-submit" class="btn btn-raised btn-success">Iniciar<div class="ripple-container"></div></button>
-                        <hr>
-                         <a class="login-link" href="#">¿No recuerda su contraseña?</a>
-                         <?php
-                                echo form_close();
-                                echo "<hr><div class='errors'>";
-                                echo validation_errors();
-                                echo "</div>";
-                         ?>
-                </div>
+                        <br>
+
+                         <a class="login-link s-top" href="#">¿No recuerda su contraseña?</a>
+<?php
+echo form_close();
+echo "<br><div class='errors'>";
+echo validation_errors();
+echo "</div>";
+?>
+</div>
         </div>
 </div>
