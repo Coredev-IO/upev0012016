9cd01fa879630a997a1f812643b71e3469fef9c5
Octavio Martinez Campuzano <octavio.martinez@coredev.io>
2016-08-02 16:25:09 -0500

    Recursos locales

--- application/views/includes/js.php
+++ application/views/includes/js.php
@@ -1,7 +1,7 @@
-<script src="//code.jquery.com/jquery-1.10.2.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js"></script>
-<script src="http://fezvrasta.github.io/bootstrap-material-design/dist/js/material.js"></script>
-<script src="http://fezvrasta.github.io/bootstrap-material-design/dist/js/ripples.min.js"></script>
+<script src="<?=base_url()?>/principal/js/jquery-1.10.2.min.js"></script>
+<script src="<?=base_url()?>/principal/js/bootstrap.min.js"></script>
+<script src="<?=base_url()?>/principal/js/material.js"></script>
+<script src="<?=base_url()?>/principal/js/ripples.min.js"></script>
 <script type="text/javascript">
         $.material.init();
 
