ca30fa2370d645ed583648433808d9367171b833
Octavio Martinez Campuzano <octavio.martinez@coredev.io>
2016-08-14 03:37:57 -0500

    MEnu salir

---
+++ application/views/includes/salir.php
@@ -0,0 +1,14 @@
+<div class="well  col-md-12 menu-res">
+      <div class="col-md-6" id="input" style="display: block;">
+      <label class="header"><?php echo "USUARIO: ".strtoupper($datos['Userdisplay'])."<br>UNIDAD: ".$datos['NombreUnidad']."<br>CORREO: ".$datos['Email']?></label>
+      </div>
+
+      <div class="col-md-6 salir-btn" id="input" style="display: block;">
+      <label class="header"><a class="btn btn-success" href="<?=base_url()?>index.php/logout">SALIR</a></label>
+      </div>
+
+
+
+
+
+</div>
--- application/views/includes/template_consultasup.php
+++ application/views/includes/template_consultasup.php
@@ -3,51 +3,12 @@
 <?php $this->load->view('includes/head')?>
         <?php $this->load->view(('includes/header-ipn'))?>
         <?php $this->load->view('includes/menu-top')?>
-
 <div class="container cnt-body">
 
 
 
-<div class="well page active  col-md-12 menu-res">
-      <div class="col-md-12" id="input" style="display: block;">
-      <label class="header"><?php echo "USUARIO: ".strtoupper($datos['Userdisplay'])."<br>UNIDAD: ".$datos['NombreUnidad']."<br>CORREO: ".$datos['Email']?></label>
-      </div>
-
-
-
-
-
-</div>
-
-
-                 <!-- Navbar principal -->
-					<div class="bs-component">
-					        <div class="navbar navbar-default">
-					                <div class="container-fluid">
-					                        <div class="navbar-header">
-					                                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
-					                                        <span class="icon-bar"></span>
-					                                        <span class="icon-bar"></span>
-					                                        <span class="icon-bar"></span>
-					                                </button>
-					                                <!-- <a class="navbar-brand" href="javascript:void(0)">UPEV</a> -->
-					                        </div>
-					                        <div class="navbar-collapse collapse navbar-responsive-collapse">
-					                                <ul class="nav navbar-nav">
-					                                </ul>
-					                                <ul class="nav navbar-nav navbar-right">
-					                                        <li><a href="<?=base_url()?>index.php/logout">SALIR</a></li>
-					                                </ul>
-					                        </div>
-					                </div>
-					        </div>
-					        <div id="source-button" class="btn btn-primary btn-xs" style="display: none;">&lt;
- &gt;
-</div>
-					</div>
-
-
-                 		<div id="app">
+<?php $this->load->view(('includes/salir'))?>
+<div id="app">
                  		<div class="">
                  				<div class="row">
 
