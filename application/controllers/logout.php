b28885d039e71d1bf1bf8d2efb84ada5c0f8e101
Octavio Martinez Campuzano <octavio.martinez@coredev.io>
2016-08-17 02:59:48 -0500

    sesion

--- application/controllers/logout.php
+++ application/controllers/logout.php
@@ -8,8 +8,8 @@ class Logout extends CI_Controller {
 	}
 
 	public function index() {
-                // $this->session->unset_userdata('logged_in');
-                session_destroy();
+                $this->session->unset_userdata('logged_in');
+                // session_destroy();
                 redirect('login', 'refresh');
 	}
 }
